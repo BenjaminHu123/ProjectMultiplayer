5c62057d490fc2fc03f1d9f869af52d01e5f20a2
BenjaminHu123 <43998171+BenjaminHu123@users.noreply.github.com>
2019-04-11 11:19:35 -0400

    general set up

---
+++ Teamwork/Assets/Scripts/PhotonLobby.cs
@@ -0,0 +1,65 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonLobby : MonoBehaviourPunCallbacks
+{
+    public static PhotonLobby lobby;
+
+    public GameObject battleButton;
+    public GameObject cancelButton;
+
+    private void Awake()
+    {
+        lobby = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();//connect to photon server
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("player has joined");
+        battleButton.SetActive(true);
+    }
+
+    public void OnBattleButtonClicked() {
+        battleButton.SetActive(false);
+        cancelButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to join, but failed --> no open games");
+        CreateRoom();
+    }
+
+    void CreateRoom() {
+        int randomRoomName = Random.Range(0, 10000);
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 10 };
+        PhotonNetwork.CreateRoom("Room" + randomRoomName, roomOps);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to create room --> another room with same name");
+        CreateRoom();
+    }
+    public void onCancelButtonClicked()
+    {
+        cancelButton.SetActive(false);
+        battleButton.SetActive(true);
+        PhotonNetwork.LeaveRoom();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
